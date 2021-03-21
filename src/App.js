7350a024e815fcfb45595245f0625dbb9c72bd59
Nico Albanese <gcalbanese96@gmail.com>
2021-03-21 10:14:19 +0000

    done the challenge

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import React from 'react';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
+let styles = {
+  height: '40%',
+  width: '100%',
+  background:'hsl(195deg, 20%, 86%)',
+  position: 'fixed',
+  bottom: '0'
+};
+
 function App() {
   return (
     <>
+      <div style={styles}></div>
       <CharacterEditor />
       <Footer />
     </>
